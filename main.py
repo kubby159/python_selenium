78d015e1f81b5b9fc67e86585f2ca29c92cdd838
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-23 01:10:11 +0900

    네이버 접속 후 앞뒤 페이지 전환

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
 import time
-chrome = webdriver.Chrome("/opt/homebrew/bin/chromedriver")
-chrome.get("http://blog.naver.com/lynn1128") # 사이트 접속
-time.sleep(2)
+
+
+options = webdriver.ChromeOptions()
+options.add_argument('--window-size=100,100')  # 공백주의!
+options.add_argument('no-sandbox') # 샌드박스란? 
+# options.add_argument('headless') # 크롬창을 안보이게 함.
+chrome = webdriver.Chrome("/opt/homebrew/bin/chromedriver",options = options)
+chrome.get("https://naver.com") # 사이트 접속, get 은 '이동한다' 는 의미.
+chrome.get('https://shopping.naver.com')
+chrome.back()
+chrome.forward()
+time.sleep(5)
 chrome.close()
